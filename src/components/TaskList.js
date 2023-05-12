6bbfadc3110860768fbeb8e7f69ef6719d0a025c
tiagoorlamunder <tiago.orlamunder@gmail.com>
2023-05-11 22:10:04 -0300

    Add calendar

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,6 +4,7 @@ import './TaskList.css';
 
 function TaskList() {
   const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState(null);
 
   const handleAddTask = () => {
     setTasks([...tasks, { text: "", subtasks: [] }]);
@@ -39,8 +40,25 @@ function TaskList() {
     setTasks(newTasks);
   };
 
+  const handleCalendarButtonClick = (taskIndex, isSubtask, subtaskIndex = null) => {
+    setSelectedTask({ taskIndex, isSubtask, subtaskIndex });
+  };
+
+  const renderCalendar = () => {
+    if (selectedTask === null) return null;
+    return (
+      <div className="calendar-container">
+        <h3>Alterar data de início e fim</h3>
+        <input type="date" />
+        <input type="date" />
+        <button onClick={() => setSelectedTask(null)}>Fechar</button>
+      </div>
+    );
+  };
+
   return (
     <div className="task-list">
+      {selectedTask && <div className="overlay" />}
       <div className="task-list-header">
         <h2>Lista de Tarefas</h2>
         <button onClick={handleAddTask}>Adicionar tarefa</button>
@@ -55,9 +73,24 @@ function TaskList() {
               onChange={(event) => handleTaskChange(event, index)}
             />
             <div className="task-buttons">
-              <button className="add-subtask-button" onClick={() => handleAddSubtask(index)}><AiOutlinePlus /></button>
-              <button className="add-subtask-button" onClick={() => handleRemoveTask(index)}><AiFillCalendar  /></button>
-              <button className="remove-task-button" onClick={() => handleRemoveTask(index)}><AiFillDelete /></button>
+              <button
+                className="add-subtask-button"
+                onClick={() => handleAddSubtask(index)}
+              >
+                <AiOutlinePlus />
+              </button>
+              <button
+                className="add-subtask-button"
+                onClick={() => handleCalendarButtonClick(index, false)}
+              >
+                <AiFillCalendar />
+              </button>
+              <button
+                className="remove-task-button"
+                onClick={() => handleRemoveTask(index)}
+              >
+                <AiFillDelete />
+              </button>
             </div>
           </div>
           {task.subtasks.map((subtask, subtaskIndex) => (
@@ -66,17 +99,36 @@ function TaskList() {
                 type="text"
                 placeholder={`Subtarefa ${subtaskIndex + 1}`}
                 value={subtask}
-                onChange={(event) => handleSubtaskChange(event, index, subtaskIndex)}
+                onChange={(event) =>
+                  handleSubtaskChange(event, index, subtaskIndex)
+                }
               />
               <div className="subtask-buttons">
-                <button className="add-subtask-button" onClick={() => handleRemoveTask(index)}><AiFillCalendar  /></button>
-                <button className="remove-subtask-button" onClick={() => handleRemoveSubtask(index, subtaskIndex)}><AiFillDelete /></button>
+                <button
+                  className="add-subtask-button"
+                  onClick={() =>
+                    handleCalendarButtonClick(index, true, subtaskIndex)
+                  }
+                >
+                  <AiFillCalendar />
+                </button>
+                <button
+                  className="remove-subtask-button"
+                  onClick={() => handleRemoveSubtask(index, subtaskIndex)}
+                >
+                  <AiFillDelete />
+                </button>
               </div>
             </div>
           ))}
-          {index < tasks.length - 1 && <div className="task-separator"><hr /></div>}
+          {index < tasks.length - 1 && (
+            <div className="task-separator">
+              <hr />
+            </div>
+          )}
         </div>
       ))}
+      {renderCalendar()}
     </div>
   );
 }
